89d86be182cec4797fa5fc6f28e0def509c3a153
Sam <75541599+Sam-3-git@users.noreply.github.com>
2024-06-04 23:00:50 +0100

    add setup

---
+++ Scripts/Custom_LocalAdminGroup/Custom_LocalAdminGroupSetup.ps1
@@ -0,0 +1,9 @@
+###############################################################################
+# # Author: Sam                                                             # #
+# # Script: SUG-Toolbox.ps1                                                 # #
+# # Date:   60/04/2024                                                      # #
+# # Goal:   To setup ci items and baselines for adding                      # #
+##          hardwareclass to clients                                        # #
+# # Version: 2.5                                                            # #
+###############################################################################
+
